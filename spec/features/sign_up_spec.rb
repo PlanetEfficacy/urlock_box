c33cf426da11a34c396271b2bc1f9d638ff7955b
PlanetEfficacy <jspevack@gmail.com>
2016-12-19 12:10:00 -0700

    Rename test, tests run now

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -13,7 +13,7 @@ RSpec.feature "Sign up", type: feature do
     expect(page).to have_content("andrew@example.com")
   end
 
-  scenario "Unauthenticated user completes form with already registered email" do
+  xscenario "Unauthenticated user completes form with already registered email" do
     user = create :user, email: "andrew@example.com"
     visit root
     click_link "Sign Up"
@@ -25,7 +25,7 @@ RSpec.feature "Sign up", type: feature do
     expect(current_path).to eq(registration_path)
     expect(page).to have_content("There is an account already associated with that email address.")
   end
-  scenario "Unauthenticated user completes form" do
+  xscenario "Unauthenticated user completes form" do
     visit root
     click_link "Sign Up"
     fill_in "Email", with: "andrew@example.com"
