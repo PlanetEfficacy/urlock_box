0835c7a6ef282ab03679e0349e28b359b1f906a2
PlanetEfficacy <jspevack@gmail.com>
2016-12-19 13:02:04 -0700

    Add test for bad passwords

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe "Sign up" do
     expect(current_path).to eq(new_registration_path)
     expect(page).to have_content("Email has already been taken")
   end
-  xscenario "Unauthenticated user completes form" do
+  scenario "Unauthenticated user completes form" do
     visit root_path
     click_link "Sign up"
     fill_in "Email", with: "andrew@example.com"
@@ -34,6 +34,6 @@ RSpec.describe "Sign up" do
     click_button "Sign up"
 
     expect(current_path).to eq(new_registration_path)
-    expect(page).to have_content("Password and password confirmation do not match.")
+    expect(page).to have_content("Password confirmation doesn't match Password")
   end
 end
