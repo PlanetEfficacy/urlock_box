e3ac13e441c9616c12e43cf296fe61a9dee8096c
PlanetEfficacy <jspevack@gmail.com>
2016-12-19 20:13:19 -0700

    Add update all attributes

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -17,7 +17,16 @@ class Api::V1::LinksController < ApplicationController
   end
 
   def update
-    
+    @link = Link.update(params[:id],
+                        title: params[:link][:title],
+                        url: params[:link][:url],
+                        status: params[:link][:status])
+    if @link
+      render json: { id: @link.id,
+                     title: @link.title,
+                     url: @link.url,
+                     status: @link.status }
+    end
   end
 
   private
